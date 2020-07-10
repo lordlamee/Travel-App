a5af704d6f2bfdea4f41c8caa5a979e832a44c16
lordlamee <gabrielolamideakorede@gmail.com>
2020-07-10 21:06:28 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
